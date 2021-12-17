29f2f3a59adbe05007957c40bba259c894528c10
danielt98 <detulande@unicauca.edu.co>
2021-12-16 22:48:23 -0500

    Contraseña os.

--- satoriBack/settings/development.py
+++ satoriBack/settings/development.py
@@ -33,6 +33,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'deliveryfoodpopayan@gmail.com'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = 'Delivery123'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
