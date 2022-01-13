d255cb9d2350e0de84da89223b537d0a7955da2c
danielt98 <detulande@unicauca.edu.co>
2022-01-12 19:55:29 -0500

    Modelo cursos

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, Payment
+
+@admin.register(Course)
+class coursesAdmin(admin.ModelAdmin):
+    list_display=('coTitle',)
+    list_filter=('coTitle',)
+    search_fields=('coTitle',)
+@admin.register(Payment)
+class paymentAdmin(admin.ModelAdmin):
+    list_display=('paDate',)
+    list_filter=('paDate',)
+    search_fields=('paDate',)
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.db.models import (
+    CASCADE,
+    Model,
+    ForeignKey
+)
+from django.db.models.deletion import DO_NOTHING
+from django.utils.translation import gettext_lazy as _
+from users.models import UserStori
 
-# Create your models here.
+class Course(Model):
+    """Cousers model."""
+    coTitle = models.CharField(_('Titulo'), max_length=255,blank=False)
+    coDescription = models.CharField(_('Description'), max_length=255,blank=False)
+    coImage =models.CharField(_('Email'),max_length=255,blank=False)
+    coPrice = models.CharField(_('Precio'),max_length=255,blank=False)
+    members = models.ManyToManyField(UserStori, through='Payment')
+    class Meta:
+        verbose_name = _('Curso')
+        verbose_name_plural = _('Cursos')
+
+    def __str__(self):
+        return self.coTitle
+class Payment(Model):
+    user = ForeignKey(
+        UserStori,
+        on_delete=DO_NOTHING,
+    )
+    course = ForeignKey(
+        Course,
+        on_delete=DO_NOTHING,
+    )
+    paDate = models.DateField(_('Fecha'),blank=False)
+
+    class Meta:
+        verbose_name = _('Payment')
+        verbose_name_plural = _('Payments')
